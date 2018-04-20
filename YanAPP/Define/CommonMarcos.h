ca101b0e50d2348398cdefc3998d95f90b7e4a6c
micyo202 <micyo202@163.com>
2018-04-20 17:42:18 +0800

    添加通用宏

---
+++ YanAPP/Define/CommonMarcos.h
@@ -0,0 +1,54 @@
+/************************************************************
+ Class    : CommonMarcos.h
+ Describe : 通用宏
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2018-04-19
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
+
+#ifndef CommonMarcos_h
+#define CommonMarcos_h
+
+// 在控制台输出Log日志
+#ifdef  DEBUG
+#define DLog(FORMAT, ...) NSLog((@"Yan输出[Debug Log]%s [Line %d] " FORMAT), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define DLog(...)
+#endif
+
+// 单例模式方法
+#define SingletonH(name) + (instancetype)shared##name;
+
+#define SingletonM(name) \
+static id _instance; \
+\
++ (instancetype)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instance = [super allocWithZone:zone]; \
+}); \
+return _instance; \
+} \
+\
++ (instancetype)shared##name \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instance = [[self alloc] init]; \
+}); \
+return _instance; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instance; \
+}\
+\
+- (id)mutableCopyWithZone:(NSZone *)zone { \
+return _instance; \
+}
+
+#endif /* CommonMarcos_h */

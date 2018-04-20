62e0cc5e652257c2f74806f77159ba646e096492
micyo202 <micyo202@163.com>
2018-04-20 17:41:50 +0800

    添加工具类宏

---
+++ YanAPP/Define/UtilsMarcos.h
@@ -0,0 +1,18 @@
+/************************************************************
+ Class    : ScreenMarcos.h
+ Describe : 工具类宏
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2018-04-20
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
+
+#ifndef UtilsMarcos_h
+#define UtilsMarcos_h
+
+#define APPLICATION     UIApplication.sharedApplication
+#define DELEGATE        UIApplication.sharedApplication.delegate
+#define WINDOW          UIApplication.sharedApplication.delegate.window
+
+#endif /* UtilsMarcos_h */

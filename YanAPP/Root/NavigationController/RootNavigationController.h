9a141b8e9b4ef4a1fbea66a57d53e033153c3e45
micyo202 <micyo202@163.com>
2018-04-20 17:08:50 +0800

    添加根导航栏

---
+++ YanAPP/Root/NavigationController/RootNavigationController.h
@@ -0,0 +1,15 @@
+/************************************************************
+ Class    : RootNavigationController.h
+ Describe : 根导航栏控制器，继承于 RTRootNavigationController
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2018-04-17
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
+
+#import "RTRootNavigationController.h"
+
+@interface RootNavigationController : RTRootNavigationController
+
+@end
---
+++ YanAPP/Root/NavigationController/RootNavigationController.m
@@ -0,0 +1,47 @@
+/************************************************************
+ Class    : RootNavigationController.m
+ Describe : 根导航栏控制器，继承于 RTRootNavigationController
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2018-04-17
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
+
+#import "RootNavigationController.h"
+
+@interface RootNavigationController ()
+
+@end
+
+@implementation RootNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma mark - 重写pushViewController:方法
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    if(self.viewControllers.count > 0) {
+        [viewController setHidesBottomBarWhenPushed:YES];   // 隐藏底部tabBar
+    }
+    [super pushViewController:viewController animated:animated];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

878a5e22ff4bf8a8563b2837ca5288c9f4c2e159
micyo202 <micyo202@163.com>
2018-04-20 17:33:45 +0800

    应用程序入口main

---
+++ YanAPP/Supporting Files/main.m
@@ -0,0 +1,18 @@
+/************************************************************
+ Class    : main.m
+ Describe : 应用程序入口
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2018-04-17
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

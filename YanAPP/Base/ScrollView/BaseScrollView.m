a9c16935ee4b2275f5821851b8b8aad27617e41c
micyo202 <micyo202@163.com>
2018-04-21 23:05:32 +0800

    更新基本的滚动视图

--- YanAPP/Base/ScrollView/BaseScrollView.m
+++ YanAPP/Base/ScrollView/BaseScrollView.m
@@ -3,7 +3,7 @@
  Describe : 基本的滚动视图，带边缘回弹效果
  Company  : Micyo
  Author   : Yanzheng 严正
- Date     : 2018-04-19
+ Date     : 2018-04-21
  Version  : 1.0
  Declare  : Copyright © 2018 Yanzheng. All rights reserved.
  ************************************************************/

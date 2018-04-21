5208e4365d1e2c519fba4c3b4738c2b50de22d32
micyo202 <micyo202@163.com>
2018-04-21 23:03:27 +0800

    更新根TabBar

--- YanAPP/Root/TabBarController/RootTabBarController.m
+++ YanAPP/Root/TabBarController/RootTabBarController.m
@@ -1,7 +1,7 @@
 /************************************************************
  Class    : RootTabBarController.h
  Describe : 根选项卡控制器，也是整个程序的root视图
- Company  : Prient
+ Company  : Micyo
  Author   : Yanzheng 严正
  Date     : 2018-04-17
  Version  : 1.0

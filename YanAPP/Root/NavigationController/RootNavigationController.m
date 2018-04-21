9ed234c5907f70bce4ec49dba2d768c5303b5193
micyo202 <micyo202@163.com>
2018-04-21 23:03:04 +0800

    更新根导航栏

--- YanAPP/Root/NavigationController/RootNavigationController.m
+++ YanAPP/Root/NavigationController/RootNavigationController.m
@@ -1,7 +1,7 @@
 /************************************************************
  Class    : RootNavigationController.m
  Describe : 根导航栏控制器，继承于 RTRootNavigationController
- Company  : Prient
+ Company  : Micyo
  Author   : Yanzheng 严正
  Date     : 2018-04-17
  Version  : 1.0

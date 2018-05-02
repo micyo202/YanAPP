81840bc3b08a4936bb849e8652d415c234b45bc8
micyo202 <micyo202@163.com>
2018-05-02 19:10:23 +0800

    添加我的模块模型

--- YanAPP/Classes/Modules/Mine/Model/MineModel.h
+++ YanAPP/Classes/Modules/Mine/Model/MineModel.h
@@ -1,14 +1,36 @@
-//
-//  MineModel.h
-//  YanAPP
-//
-//  Created by Apple on 2018/4/30.
-//  Copyright © 2018年 Yanzheng. All rights reserved.
-//
+/************************************************************
+ Class    : MineModel.h
+ Describe : 我的模块数据模型
+ Company  : Micyo
+ Author   : Yanzheng 严正
+ Date     : 2018-04-30
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import <Foundation/Foundation.h>
 
+#pragma mark - 自定义枚举类型
+#pragma mark 对齐方式ENUM
+typedef NS_ENUM(NSInteger, MineModelItemAlignment) {
+    MineModelItemAlignmentLeft,
+    MineModelItemAlignmentRight,
+    MineModelItemAlignmentMiddle
+};
+
+#pragma mark 类型ENUM
+typedef NS_ENUM(NSInteger, MineModelItemType) {
+    MineModelItemTypeDefault,   // image, title, rightTitle, rightImage
+    MineModelItemTypeButton,    // button
+    MineModelItemTypeSwitch     // title， switch
+};
+
 @interface MineModelItem : NSObject
+
+// 对齐方式
+@property (nonatomic, assign) MineModelItemAlignment alignment;
+// 类型
+@property (nonatomic, assign) MineModelItemType type;
 
 /************************ 属性 ************************/
 // 1 左边图标（主图片）
@@ -29,10 +51,48 @@
 // 6 btton、switch的标签tag
 @property (nonatomic, assign) NSInteger tag;
 // 7 设置switch的值
-@property (nonatomic, assign) BOOL isOn;
+@property (nonatomic, assign, getter=isOn) BOOL on;
+
+/************************ 样式 ************************/
+@property (nonatomic, assign) UITableViewCellAccessoryType accessoryType;
+@property (nonatomic, assign) UITableViewCellSelectionStyle selectionStyle;
+
+@property (nonatomic, strong) UIColor *bgColor;
+@property (nonatomic, strong) UIColor *btnBGColor;
+@property (nonatomic, strong) UIColor *btnTitleColor;
+
+@property (nonatomic, strong) UIColor *titleColor;
+@property (nonatomic, strong) UIFont *titleFont;
+
+@property (nonatomic, strong) UIColor *subTitleColor;
+@property (nonatomic, strong) UIFont *subTitleFont;
+
+@property (nonatomic, assign) CGFloat rightImageHeightOfCell;
+@property (nonatomic, assign) CGFloat middleImageHeightOfCell;
+
+/************************ 类方法 ************************/
++ (MineModelItem *) createWithTitle:(NSString *)title;
++ (MineModelItem *) createWithImageName:(NSString *)imageName title:(NSString *)title;
++ (MineModelItem *) createWithTitle:(NSString *)title subTitle:(NSString *)subTitle;
++ (MineModelItem *) createWithImageName:(NSString *)imageName title:(NSString *)title middleImageName:(NSString *)middleImageName subTitle:(NSString *)subTitle;
++ (MineModelItem *) createWithImageName:(NSString *)imageName title:(NSString *)title subTitle:(NSString *)subTitle rightImageName:(NSString *)rightImageName;
++ (MineModelItem *) createWithImageName:(NSString *)imageName title:(NSString *)title middleImageName:(NSString *)middleImageName subTitle:(NSString *)subTitle rightImageName:(NSString *)rightImageName;
 
 @end
 
 @interface MineModelGroup : NSObject
+
+/************************ 属性 ************************/
+// 组头部标题
+@property (nonatomic, copy) NSString *headerTitle;
+// 组尾部说明
+@property (nonatomic, copy) NSString *footerTitle;
+// 组元素
+@property (nonatomic, strong) NSMutableArray *items;
+@property (nonatomic, assign, readonly) NSUInteger itemsCount;
+
+/************************ 类方法 ************************/
+- (instancetype) initWithHeaderTitle:(NSString *)headerTitle footerTitle:(NSString *)footerTitle settingItems:(MineModelItem *)firstObj, ...;
+- (MineModelItem *) itemAtIndex:(NSUInteger)index;
 
 @end
--- YanAPP/Classes/Modules/Mine/Model/MineModel.m
+++ YanAPP/Classes/Modules/Mine/Model/MineModel.m
@@ -1,17 +1,123 @@
-//
-//  MineModel.m
-//  YanAPP
-//
-//  Created by Apple on 2018/4/30.
-//  Copyright © 2018年 Yanzheng. All rights reserved.
-//
+/************************************************************
+ Class    : MineModel.m
+ Describe : 我的模块数据模型
+ Company  : Micyo
+ Author   : Yanzheng 严正
+ Date     : 2018-04-30
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import "MineModel.h"
 
 @implementation MineModelItem
 
+#pragma mark 初始化方法
+- (instancetype) init{
+    if (self = [super init]) {
+        self.alignment = MineModelItemAlignmentLeft;
+        
+        self.bgColor = [UIColor whiteColor];
+        self.titleColor = [UIColor blackColor];
+        self.subTitleColor = [UIColor grayColor];
+        self.titleFont = [UIFont systemFontOfSize:17.0f];
+        self.subTitleFont = [UIFont systemFontOfSize:15.0f];
+        
+        self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        self.selectionStyle = UITableViewCellSelectionStyleDefault;
+        self.rightImageHeightOfCell = 0.72f;
+        self.middleImageHeightOfCell = 0.35f;
+    }
+    return self;
+}
+
+#pragma mark 创建item的各种方法
++ (MineModelItem *) createWithTitle:(NSString *)title{
+    return [MineModelItem createWithImageName:nil title:title];
+}
+
++ (MineModelItem *) createWithImageName:(NSString *)imageName title:(NSString *)title{
+    return [MineModelItem createWithImageName:imageName title:title subTitle:nil rightImageName:nil];
+}
+
++ (MineModelItem *) createWithTitle:(NSString *)title subTitle:(NSString *)subTitle{
+    return [MineModelItem createWithImageName:nil title:title subTitle:subTitle rightImageName:nil];
+}
+
++ (MineModelItem *) createWithImageName:(NSString *)imageName title:(NSString *)title middleImageName:(NSString *)middleImageName subTitle:(NSString *)subTitle{
+    return [MineModelItem createWithImageName:imageName title:title middleImageName:middleImageName subTitle:subTitle rightImageName:nil];
+}
+
++ (MineModelItem *) createWithImageName:(NSString *)imageName title:(NSString *)title subTitle:(NSString *)subTitle rightImageName:(NSString *)rightImageName{
+    return [MineModelItem createWithImageName:imageName title:title middleImageName:nil subTitle:subTitle rightImageName:rightImageName];
+}
+
++ (MineModelItem *) createWithImageName:(NSString *)imageName title:(NSString *)title middleImageName:(NSString *)middleImageName subTitle:(NSString *)subTitle rightImageName:(NSString *)rightImageName{
+    MineModelItem *item = [[MineModelItem alloc] init];
+    item.imageName = imageName;
+    item.middleImageName = middleImageName;
+    item.rightImageName = rightImageName;
+    item.title = title;
+    item.subTitle = subTitle;
+    return item;
+}
+
+#pragma mark - 设置对齐方式
+- (void) setAlignment:(MineModelItemAlignment)alignment{
+    _alignment = alignment;
+    if (alignment == MineModelItemAlignmentMiddle) {
+        self.accessoryType = UITableViewCellAccessoryNone;
+    }
+}
+
+#pragma mark - 设置类型
+- (void) setType:(MineModelItemType)type{
+    _type = type;
+    if (type == MineModelItemTypeSwitch) {
+        self.accessoryType = UITableViewCellAccessoryNone;
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    else if (type == MineModelItemTypeButton) {
+        self.btnBGColor = DEFAULT_RED_COLOR;
+        self.btnTitleColor = [UIColor whiteColor];
+        self.accessoryType = UITableViewCellAccessoryNone;
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        self.bgColor = [UIColor clearColor];
+    }
+}
+
 @end
 
 @implementation MineModelGroup
+
+#pragma mark 初始化一个组
+- (instancetype) initWithHeaderTitle:(NSString *)headerTitle footerTitle:(NSString *)footerTitle settingItems:(MineModelItem *)firstObj, ...{
+    if (self = [super init]) {
+        _headerTitle = headerTitle;
+        _footerTitle = footerTitle;
+        _items = [[NSMutableArray alloc] init];
+        va_list argList;
+        if (firstObj) {
+            [_items addObject:firstObj];
+            va_start(argList, firstObj);
+            id arg;
+            while ((arg = va_arg(argList, id))) {
+                [_items addObject:arg];
+            }
+            va_end(argList);
+        }
+    }
+    return self;
+}
+
+#pragma mark 根据下标获取对象
+- (MineModelItem *) itemAtIndex:(NSUInteger)index{
+    return [_items objectAtIndex:index];
+}
+
+#pragma mark 获取每组中对象的个数
+- (NSUInteger) itemsCount{
+    return self.items.count;
+}
 
 @end

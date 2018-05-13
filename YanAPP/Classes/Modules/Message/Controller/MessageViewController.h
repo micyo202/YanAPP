903b1d1bdfd44d2cc17a332e87a6bcff769eb370
micyo202 <micyo202@163.com>
2018-05-13 12:00:12 +0800

    更新消息模块视图控制器

---
+++ YanAPP/Classes/Modules/Message/Controller/MessageViewController.h
@@ -0,0 +1,15 @@
+/************************************************************
+ Class    : MessageViewController.h
+ Describe : 消息模块视图控制器
+ Company  : Micyo
+ Author   : Yanzheng 严正
+ Date     : 2018-04-19
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
+
+#import "BaseViewController.h"
+
+@interface MessageViewController : BaseViewController
+
+@end
---
+++ YanAPP/Classes/Modules/Message/Controller/MessageViewController.m
@@ -0,0 +1,42 @@
+/************************************************************
+ Class    : MessageViewController.m
+ Describe : 消息模块视图控制器
+ Company  : Micyo
+ Author   : Yanzheng 严正
+ Date     : 2018-04-19
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
+
+#import "MessageViewController.h"
+
+@interface MessageViewController ()
+
+@end
+
+@implementation MessageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.navigationItem.title = @"消息";
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
